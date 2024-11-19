e295afb1d3742325ceb00a0e5d952e955957551a
Touyer Lee <touyer.lees@gmail.com>
2024-11-19 13:32:24 -0600

    GLAB 303.3.2 - Reading a String From The Console

---
+++ src/ReadingStringFromConsole.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReadingStringFromConsole {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three words separated by spaces: ");
+        String s1 = input.next();
+        String s2 = input.next();
+        String s3 = input.next();
+        System.out.println("s1 is " + s1);
+        System.out.println("s2 is " + s2);
+        System.out.println("s3 is " + s3);
+
+
+        //Example 2: Reading a String from the Console
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        String s = input2.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+
+    }
+}
